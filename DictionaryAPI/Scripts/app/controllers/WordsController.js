60a37547998484661964212c6bee9c3d50efae10
Jonathan Soto <jsotom@psl.com.co>
2015-02-27 08:56:06 -0500

    WordsController greater improved: JQuery dependency removed (this was the only part where it was used); areEqual() method removed since the comparison is simpler now; saveEditedWord() method simplified

--- DictionaryAPI/Scripts/app/controllers/WordsController.js
+++ DictionaryAPI/Scripts/app/controllers/WordsController.js
@@ -3,78 +3,40 @@
     _self.isAbleToAddWord = true;
     _self.search;
 
+    var defaultName = 'WordToDefine';
+    var defaultDefinition = 'Write your definition here';
+
     getWords(function (returnedWords) {
         _self.words = returnedWords;
     });
 
     // private methods
-    var getTextFromId = function (id) {
-        return $('#' + id).html().trimHtml();
-    };
+    var saveEditedWord = function (ckeditorInstance) {
+        var dividedId = ckeditorInstance.name.split('-');
+        var prefix = dividedId[0];
+        var wordAttribute = dividedId[1];
+        var wordId = dividedId[2];
+        
+        if (wordId == '') {// if the word is new
+            var newWord = _self.words.filter(function (wordItem) {
+                return wordItem.name == defaultName || wordItem.definition == defaultDefinition;
+            })[0];
 
-    var areEqual = function (word1, word2) {
-        var properties = new Array(0);
-
-        for (property in word1) {
-            if (property[0] !== '$') {
-                properties[properties.length] = property;
-            }
-        }
-
-        for (property in word2) {
-            if (property[0] !== '$') {
-                properties[properties.length] = property;
-            }
-        }
-
-        properties = properties.filter(function (elem, index, self) {
-            return index == self.indexOf(elem);
-        });
-
-        for (index in properties) {
-            if (word1[properties[index]] !== word2[properties[index]]) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    var replaceWord = function (oldWord, newWord) {
-        _self.words[_self.words.indexOf(oldWord)] = newWord;
-    };
-
-    var saveEditedWord = function (id) {
-        dividedId = id.split('-');
-
-        if (dividedId[2] == '') {// if the word is new (it does not have an id yet)
-            var newWord = {
-                name: getTextFromId(dividedId[0] + '-name-'),
-                definition: getTextFromId(dividedId[0] + '-definition-')
-            };
+            newWord[wordAttribute] = ckeditorInstance.getData().trimHtml();
 
             saveWord(newWord, function (assignedId) {
                 newWord['id'] = assignedId;
-                wordWithoutId = _self.words.filter(function (wordItem) {
-                    return !wordItem.id;
-                })[0];
-                replaceWord(wordWithoutId, newWord);
             });
 
             _self.isAbleToAddWord = true;
         } else {// if the word is not new
-            var oldWord = _self.words.filter(function (wordItem) {
-                return wordItem.id == dividedId[2];
+            var updatedWord = _self.words.filter(function (wordItem) {
+                return wordItem.id == wordId;
             })[0];
-            var updatedWord = {
-                id: parseInt(dividedId[2]),
-                name: getTextFromId(dividedId[0] + '-name-' + dividedId[2]),
-                definition: getTextFromId(dividedId[0] + '-definition-' + dividedId[2])
-            };
 
-            if (!areEqual(oldWord, updatedWord)) {
+            if (updatedWord[wordAttribute] != ckeditorInstance.getData().trimHtml()) {
+                updatedWord[wordAttribute] = ckeditorInstance.getData().trimHtml();
                 updateWord(updatedWord);
-                replaceWord(oldWord, updatedWord);
             }
         }
     };
@@ -89,8 +51,8 @@
         CKEDITOR.inline(id, {
             customConfig: '/Scripts/app/ckeditor.defaultconfig.js',
             on: {
-                blur: function () {
-                    saveEditedWord(id);
+                blur: function (event) {
+                    saveEditedWord(event.sender);
                 }
             }
         });
@@ -99,8 +61,8 @@
     _self.addWord = function () {
         _self.isAbleToAddWord = false;
         _self.words[_self.words.length] = {
-            name: 'WordToDefine',
-            definition: 'Write your definition here'
+            name: defaultName,
+            definition: defaultDefinition
         };
     };
 
