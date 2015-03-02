4e7ff795442246fb62dda54dd2a10d09960ce3c3
Jonathan Soto <jsotom@psl.com.co>
2015-03-02 16:14:12 -0500

    Added test project

---
+++ Dictionary.Tests/Api/WordsControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dictionary.Tests.Api
+{
+    [TestClass]
+    public class WordsControllerTest
+    {
+        [TestMethod]
+        public void Get_ShouldReturnAllTheWords()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
